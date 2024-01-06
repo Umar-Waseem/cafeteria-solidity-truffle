0cd7b30a1d99e8c07a0c875b6e012619add5f1e7
umarwaseemcowlar <umar.waseem@cowlar.com>
2024-01-06 20:12:14 +0500

    add item from admin

--- frontend/src/components/AdminAddItem.js
+++ frontend/src/components/AdminAddItem.js
@@ -1,18 +1,48 @@
 import React, { useState } from 'react';
 import '../App.css';
 import Navbar from './Navbar';
+import Web3 from "web3";
+import { menuContractAddress } from '../addresses';
+import menuContract from "../contractAbis/MenuManagement.json"
 
 function AdminAddItem() {
 
   const [itemName, setItemName] = useState('');
   const [itemQuantity, setItemQuantity] = useState('');
   const [itemPrice, setItemPrice] = useState('');
-  
 
-  const handleSubmit = () => { 
-    alert('Item added successfully');
+
+  const handleSubmit = () => {
+    addItemToMenu();
+  };
+
+  async function addItemToMenu() {
+    try {
+      const web3 = new Web3('http://localhost:7545');
+      const menuContractABI = menuContract.abi;
+
+      const menuManagementContract = new web3.eth.Contract(
+        menuContractABI,
+        menuContractAddress
+      );
+
+      console.log("Item Name: ", itemName);
+      console.log("Item Quantity: ", itemQuantity);
+      console.log("Item Price: ", itemPrice);
+
+      let fromAccount = localStorage.getItem('account');
+      let gas = 3000000;
+
+      console.log("from account: ", fromAccount);
+
+      const res = await menuManagementContract.methods.addItem(itemName, parseInt(itemQuantity, 10), parseInt(itemPrice, 10)).send({ from: fromAccount, gas: gas })
+      console.log("Transaction Hash: ", res.transactionHash);
+      alert("Item added successfully");
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
   }
-
 
 
   return (
@@ -52,12 +82,12 @@ function AdminAddItem() {
               required
             />
           </label>
-          
+
           <button
             style={{ backgroundColor: '#007bff', color: '#fff', padding: '10px 15px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
             type="submit"
           >
-            Add
+            Add Item
           </button>
         </form>
 
